961acb12e3f2b9e11ad3e223b081ad216ac18bb0
Zevaryx <zevaryx@gmail.com>
2021-04-18 13:18:32 -0600

    Use difflib, don't be a moron

--- perms.py
+++ perms.py
@@ -1,46 +1,41 @@
 import difflib
-from itertools import permutations
-from math import factorial
-from sys import argv
-from tqdm import tqdm
-
-string = argv[1]
+import argparse
 
 
-def get_match(perm, word):
-    return difflib.SequenceMatcher(None, perm, word).ratio()
+parser = argparse.ArgumentParser(description="Get close matches")
+parser.add_argument("strings", type=str, nargs="+", help="Strings to parse")
+parser.add_argument(
+    "-c",
+    "--cutoff",
+    dest="cutoff",
+    default=0.5,
+    type=float,
+    help="Percentage of match for match cutoff (i.e. 0.5)",
+)
+parser.add_argument(
+    "-m",
+    "--matches",
+    dest="matches",
+    default=25,
+    type=int,
+    help="How many matches to return",
+)
+
+args = parser.parse_args()
 
 
 with open("words.txt") as f:
-    words = [x.strip() for x in f.readlines()]
+    words = list(set([x.strip().lower() for x in f.readlines()]))
 
-highest_percs = {}
-highest_matches = {}
-
-total = factorial(len(string))
-
-for p in tqdm(
-    permutations(string), total=total, desc="Analyzing Permutations"
-):
-    perm = "".join(p)
-    highscore = 0
-    highest = None
-    for word in words:
-        if (score := get_match(perm, word)) > highscore:
-            highscore = score
-            highest = word
-        elif score == highscore:
-            if type(highest) is str:
-                highest = [highest, word]
-            elif type(highest) is list:
-                highest.append(word)
-    highest_percs[perm] = highscore
-    highest_matches[perm] = highest
-
-sorted_percs = {
-    k: v for k, v in sorted(highest_percs.items(), key=lambda item: item[1])
-}
-
-for i in range(10):
-    k = list(sorted_percs.keys())[i]
-    print(f"{k}: {highest_matches[k]}")
+for string in args.strings:
+    print(
+        f"\nGetting close matches for {string} (cutoff={args.cutoff}, matches={args.matches})"
+    )
+    string = string.lower()
+    print(
+        "\n".join(
+            difflib.get_close_matches(
+                string, words, n=args.matches, cutoff=args.cutoff
+            )
+        )
+    )
