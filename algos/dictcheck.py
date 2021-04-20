3d671d1aaa75a1fe1c95dfc3abd7aec40418b6b0
Zevaryx <zevaryx@gmail.com>
2021-04-20 15:58:44 -0600

    Add replit stuff, partial match stuff

--- algos/dictcheck.py
+++ algos/dictcheck.py
@@ -10,6 +10,7 @@ def menu(strings: list, words: list):
 
  1) Check direct dictionary matches
  2) Check partial dictionary matches
+ 3) Check dictionary matches of portions
 
  0) Go back
     """
@@ -25,11 +26,16 @@ def menu(strings: list, words: list):
             for k in results:
                 rstr = "\n  ".join(x for x in results[k])
                 print(f"\n{k}:\n  {rstr}")
+        elif choice == "3":
+            results = {k: dictcheck_portion(k, words) for k in strings}
+            for k in results:
+                rstr = "\n  ".join(x for x in results[k])
+                print(f"\n{k}:\n  {rstr}")
         elif choice == "0":
             break
 
 
-def dictcheck(string: str, words: list) -> list:
+def dictcheck(string: str, words: list, leave=True) -> list:
     """
     Check permutations of string for dictionary match
 
@@ -44,6 +50,7 @@ def dictcheck(string: str, words: list) -> list:
         total=total,
         desc=f"Analyzing Permutations ({string})",
         unit="words",
+        leave=leave,
     ):
         if sorted(string.lower()) == sorted(word.lower()):
             matches.append(word)
@@ -69,4 +76,28 @@ def dictcheck_partial(
         x
         for x in matches
         if all(string.count(y) <= x.count(y) for y in string)
+    ]
+
+
+def dictcheck_portion(
+    string: str, words: list, cutoff: float = 0.5, matches: int = 25
+) -> list:
+    """
+    Check string for partial dictinary matches
+
+    :param string: String to check
+    :param words: Dictionary to check against
+    :return: List of matches
+    """
+    print(f"Checking {string} for partial matches to dictionary...")
+    string = string.lower()
+    matches = difflib.get_close_matches(
+        string, words, cutoff=cutoff, n=matches
+    )
+    return [
+        x
+        for x in matches
+        if all(string.count(y) >= x.count(y) for y in string)
+        and len(string) >= len(x)
+        and not any(y not in string for y in x)
     ]
